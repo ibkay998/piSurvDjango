c0f7afeb349275911a5f49ffcc66e03a0b51e092
ibukunolouwa oyeniyi <ibukun46@gmail.com>
2022-07-29 05:28:38 +0100

    added whitenoise requirements

--- piSurv/piSurv/settings.py
+++ piSurv/piSurv/settings.py
@@ -36,6 +36,7 @@ ALLOWED_HOSTS = ['pisurv.herokuapp.com',"127.0.0.1"]
 # Application definition
 
 INSTALLED_APPS = [
+    "whitenoise.runserver_nostatic",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -49,10 +50,12 @@ INSTALLED_APPS = [
     'corsheaders',
 ]
 
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
@@ -135,7 +138,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 STATIC_URL = '/static/'
 
 # Default primary key field type

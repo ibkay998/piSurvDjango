4cb1874f64322979fce34b90e53ff60a32cd8455
ibkay998 <ibukun46@gmail.com>
2022-08-13 12:14:56 +0100

    Update settings.py

--- piSurv/piSurv/settings.py
+++ piSurv/piSurv/settings.py
@@ -28,7 +28,7 @@ if os.path.isfile(dotenv_file):
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['pisurv.herokuapp.com',"127.0.0.1"]
 
